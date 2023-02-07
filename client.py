5cd695d492dffb0861d35b4ebde17987e7f50d31
souzagoncalves85 <110495352+souzagoncalves85@users.noreply.github.com>
2023-02-07 16:48:31 -0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,96 @@
+import json
+from socket import *
+from ticTacToe import TicTacToe
+
+player = 0
+ttt = TicTacToe()
+board = ttt.createBoard()
+winner = ttt.verifyWinner(board)
+player_waiting = False
+
+def responding():
+  global player
+
+  while(1):
+    row = int(input("Digite a linha: "))
+    column = int(input("Digite a coluna: "))
+
+    pack = {
+      "player":player,
+      "row": row,
+      "column": column
+    }
+
+    client.send(json.dumps(pack).encode())
+    data = json.loads(client.recv(1024).decode())
+
+def main():
+  global player
+  global client
+  global player_waiting
+  global board
+  global winner
+
+
+  HOST = 'localhost'
+  PORT = 50001
+
+  name = ""
+  while(len(name) < 1 or len(name) > 10):
+    print("Maximo de caracteres: 10")
+    print("Minimo de caracteres: 1")
+    name = input("Digite seu nome de jogador: ")
+  
+  client = socket(AF_INET, SOCK_STREAM)
+  client.connect((HOST, PORT))
+  client.send(name.encode())
+
+  print(" ")
+  print("CONECTADO...")
+  print("AGUARDANDO OUTROS JOGADORES...")
+  print(" ")
+
+  message = client.recv(1024).decode()
+  message = json.loads(message)
+  print("Você é o player %i %s" % (int(message["player"]), "[X]" if int(message["player"]) == 1 else "[O]"))
+  print("")
+  client.send("{}".encode())
+
+  player = int(message["player"])
+  if (player == 1):
+    player_waiting = False
+  else:
+    player_waiting = True
+
+  while (not winner):
+    if (not player_waiting):
+      row, column = ttt.getInputValid(board)
+      
+      ttt.makeMovement(board, row-1, column-1, player)
+    
+      pack = {
+        "player":player,
+        "row": row,
+        "column": column
+      }
+
+      client.send(json.dumps(pack).encode())
+      player_waiting = True
+
+      ttt.printBoard(board)
+    else:
+      response = client.recv(1024).decode()
+      response = json.loads(response)
+
+      ttt.makeMovement(board, response['row']-1, response['column']-1, (player % 2) + 1)
+      print("Jogada do player %s" % ((player % 2) + 1))
+      ttt.printBoard(board)
+      player_waiting = False
+
+    
+
+    winner = ttt.verifyWinner(board)
+  
+  print("Ganhador: ", winner)
+
+main()
---
+++ ticTacToe.py
@@ -0,0 +1,71 @@
+class TicTacToe:
+  def __init__(self):
+    self.value = " "
+    self.token = ["", "X", "O"]
+
+  def createBoard(self):
+    board = [
+      [self.value, self.value, self.value],
+      [self.value, self.value, self.value],
+      [self.value, self.value, self.value]
+    ]
+
+    return board
+  
+  
+  def printBoard(self, board):
+    print("")
+    for i in range(3):
+      print(" | ".join(board[i]))
+      if(i<2):
+        print("---------")
+    print("")
+
+
+  def getInputValid(self, board):
+    row = None
+    column = None
+  
+    while True:
+      try:
+        row = int(input("Digite a linha: "))
+        column = int(input("Digite a coluna: "))
+
+        if (row  < 1 or row > 3 or column < 1 or column > 3):
+          print("Número precisa estar entre 1 e 3\n")
+          continue
+
+        if (board[row-1][column-1] != self.value):
+          print("Posição já preenchida\n")
+          continue
+        else:
+          return row, column
+      
+      except:
+        print("Digite um número inteiro")
+        continue
+  
+
+  def makeMovement(self, board, line, column, player):
+    board[line][column] = self.token[player]
+
+  def verifyWinner(self, board):
+    for i in range(3):
+      if(board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != self.value ):
+        return board[i][0]
+
+      if(board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != self.value ):
+        return board[0][i]
+
+    if(board[0][0]!= self.value and board[0][0] == board[1][1] and board[1][1] == board[2][2]):
+      return board[0][0]
+
+    if(board[0][2]!= self.value and board[0][2] == board[1][1] and board[1][1] == board[2][0]):
+      return board[0][2]
+
+    for line in board:
+      for column in line:
+        if (column == self.value):
+          return False
+
+    return "Empate"
